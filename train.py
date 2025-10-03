9ca0ceca0d16788d1c5c194792e1878c01e1a151
William Liaw <william590y@gmail.com>
2025-10-03 12:17:04 -0400

    hi

--- train.py
+++ train.py
@@ -252,7 +252,7 @@ def main():
     parser.add_argument('--output_dir', type=Path, default=Path('./fine_tuned_full'))
     parser.add_argument('--batch_size', type=int, default=32) 
     parser.add_argument('--val_batch_size', type=int, default=16)
-    parser.add_argument('--gradient_accumulation_steps', type=int, default=32)  # For effective batch size 256
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=16)  # For effective batch size 256
     parser.add_argument('--learning_rate', type=float, default=3e-5)
     parser.add_argument('--max_steps', type=int, default=3500)
     parser.add_argument('--save_steps', type=int, default=500)
