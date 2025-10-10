89dc71b309b10c32636408f178689fc1dd8ca3b2
William Liaw <william590y@gmail.com>
2025-10-10 06:06:59 -0400

    changes made

--- train.py
+++ train.py
@@ -153,8 +153,8 @@ def main():
     parser.add_argument('--val_file', type=Path, default=Path('./data/test_output.txt'))
     parser.add_argument('--model_name', type=str, default='stanford-crfm/music-medium-800k')
     parser.add_argument('--output_dir', type=Path, default=Path('./fine_tuned_full'))
-    parser.add_argument('--batch_size', type=int, default=8) 
-    parser.add_argument('--val_batch_size', type=int, default=16)
+    parser.add_argument('--batch_size', type=int, default=32) 
+    parser.add_argument('--val_batch_size', type=int, default=8)
     parser.add_argument('--gradient_accumulation_steps', type=int, default=16)  # For effective batch size 128
     parser.add_argument('--learning_rate', type=float, default=3e-5)
     parser.add_argument('--max_steps', type=int, default=3500)
