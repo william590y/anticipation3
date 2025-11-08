240bd7b45279a087bdd3bc79c2fef70ec0eee0f7
William Liaw <william590y@gmail.com>
2025-11-08 10:23:49 -0500

    new train dir stuff

--- train.py
+++ train.py
@@ -301,10 +301,10 @@ def plot_losses(train_losses, val_losses, val_accuracies, validation_steps, outp
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_file', type=Path, default=Path('./data/train_openings.txt'))
-    parser.add_argument('--val_file', type=Path, default=Path('./data/test_openings.txt'))
+    parser.add_argument('--data_file', type=Path, default=Path('./data/train_sliding.txt'))
+    parser.add_argument('--val_file', type=Path, default=Path('./data/test_sliding.txt'))
     parser.add_argument('--model_name', type=str, default='stanford-crfm/music-medium-800k')
-    parser.add_argument('--output_dir', type=Path, default=Path('./fine_tuned_openings'))
+    parser.add_argument('--output_dir', type=Path, default=Path('./fine_tuned_sliding'))
     parser.add_argument('--batch_size', type=int, default=8) 
     parser.add_argument('--val_batch_size', type=int, default=8)
     parser.add_argument('--gradient_accumulation_steps', type=int, default=32)  # For effective batch size 256
