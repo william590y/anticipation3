0271f2347844ab4ef35e1d063d2446fb753ba52c
William Liaw <william590y@gmail.com>
2026-03-02 21:24:56 -0500

    mixup

--- evaluate_muster.py
+++ evaluate_muster.py
@@ -38,8 +38,8 @@ from music21 import converter
 DEFAULT_CHECKPOINT = 'checkpoint-1750'
 TEST_FILE = 'data/test_combined.txt'
 OUTPUT_BASE = 'muster_evaluation_results'
-NUM_EXAMPLES = 10  # Randomly sample sequences
-RANDOM_SEED = 42
+NUM_EXAMPLES = 50  # Randomly sample sequences
+RANDOM_SEED = 41
 K_PREFIX = 33  # Number of control+rest pairs in prefix
 ALTERNATING_START = 4 + K_PREFIX * 6  # = 202
 
